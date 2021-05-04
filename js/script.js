990844795d0fab5f2863448763d7d70a7c4ef1f0
haokaili <>
2021-05-04 13:31:59 +0000

    added css and js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: haokai
+// Created on: May 2021
+// This is the Hello world
+
+function myButtonClicked() {
+  //this function displays "Hello, World!"
+
+  document.getElementById('hello-world').innerHTML = 'Hello, World!!!'
+}
